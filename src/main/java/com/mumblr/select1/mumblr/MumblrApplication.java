8e121c5156c50744c9a896420237ff3dafc0e5e5
Neigel Yap <neigelyap@gmail.com>
2018-03-26 12:46:39 +0800

    Initial code

---
+++ src/main/java/com/mumblr/select1/mumblr/MumblrApplication.java
@@ -0,0 +1,12 @@
+package com.mumblr.select1.mumblr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumblrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumblrApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mumblr/select1/mumblr/MumblrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mumblr.select1.mumblr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MumblrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
