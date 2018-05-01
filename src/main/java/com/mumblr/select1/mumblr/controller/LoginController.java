9014b573351e178871eddd5a639840739ccb137c
Jan David Umali <jan.david.umali@gmail.com>
2018-05-01 17:07:04 +0800

    Added sessions on login controller, session checker function, tiny fix on index.html and home.html, home.js created, removed LoginRepository

--- src/main/java/com/mumblr/select1/mumblr/controller/LoginController.java
+++ src/main/java/com/mumblr/select1/mumblr/controller/LoginController.java
@@ -1,14 +1,17 @@
 package com.mumblr.select1.mumblr.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mumblr.select1.mumblr.model.Accounts;
-import com.mumblr.select1.mumblr.repository.LoginRepository;
+import com.mumblr.select1.mumblr.repository.UserRepository;
 
 import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,27 +19,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Controller
 public class LoginController {
 	@Autowired
-	private LoginRepository lr;
+	private UserRepository ur;
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public ModelAndView find(@RequestParam("email") String email,@RequestParam("pass") String pass){
+	public ModelAndView find(@RequestParam("emailLogin") String email,@RequestParam("passLogin") String pass, HttpSession httpSession){
 		StrongPasswordEncryptor spe = new StrongPasswordEncryptor();
-//		Accounts acc = lr.findByEmailAndPass(email,spe.encryptPassword(pass));
-		Accounts acc = lr.findByEmail(email);
+
+		Accounts acc = ur.findByemail(email);
 		ModelAndView modelAndView = new ModelAndView();
 		
-//		if(acc.getEmail().isEmpty() && acc.getPass().isEmpty()){
+
 		if(acc.getEmail().isEmpty()){
 			modelAndView.setViewName("index");
 		}else{
 			if(spe.checkPassword(pass, acc.getPass())){
 				modelAndView.setViewName("home");
 				modelAndView.addObject("loginUser", acc);
+				httpSession.setAttribute("userAcc", acc);
 			}else{
 				modelAndView.setViewName("index");
 			}
 		}
 
 		return modelAndView;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/validating", method=RequestMethod.POST)
+	public boolean validate(HttpSession httpSession){
+		
+		if(httpSession.getAttribute("userAcc") != null){
+			
+		} else {
+			return false;
+		}
+
+		return true;
 	}
 }
